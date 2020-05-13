f1826cffd767eda88b2b621490a1530966a108ee
Gauthier Chaty <gauthier.chaty@gmail.com>
2020-05-13 14:45:56 +0200

    Adding cli  with config.ini file

--- minicode/cli.py
+++ minicode/cli.py
@@ -1,19 +1,46 @@
 """Console script for minicode."""
-import argparse
+import os
 import sys
 
+import configargparse
+
+import minicode
+import utils
 
 def main():
-    """Console script for minicode."""
-    parser = argparse.ArgumentParser()
-    parser.add_argument('_', nargs='*')
-    args = parser.parse_args()
+    parser = configargparse.ArgParser(default_config_files=[os.path.join(utils.DIRNAME, 'config.ini')])
 
-    print("Arguments: " + str(args._))
-    print("Replace this message by putting your code into "
-          "minicode.cli.main")
+    parser.add('--width', required=False, type=int, help='SVG width')
+    parser.add('--height', required=False, type=int, help='SVG height')
+    parser.add('--theme', required=False, type=str, help='Syntax highlighting theme')
+    parser.add('--input', required=False, type=str, help='Input filepath')
+    parser.add('--output', required=False, type=str, help='Output filepath')
+
+    parser.add('--code_width', required=False, type=int, help='Width in pixel of character')
+    parser.add('--code_height', required=False, type=int, help='Height in pixel of character')
+    parser.add('--start_position', required=False, type=int, help='Starting pixel position')
+    parser.add('--radius_rect', required=False, type=int, help='Radius for rounded corner')
+    parser.add('--line_spacing', required=False, type=int, help='Line spacing')
+
+    parser.add('--auto_size', required=False, action='store_true', default=True)
+    # TODO : Add Random
+    # TODO : Config File
+    # TODO : URL
+
+    options = parser.parse_args()
+    print(options)
+
+    with open(options.input, 'r') as infile:
+        code_lines = infile.readlines()
+
+    if options.auto_size:
+        options.width = (max(len(line) for line in code_lines) * options.code_width) + options.start_position
+        options.height = (len(code_lines) + 1) * (options.code_height + options.line_spacing)
+
+    minicode.generate_svg(code_lines, options)
+
     return 0
 
 
-if __name__ == "__main__":
-    sys.exit(main())  # pragma: no cover
+if __name__ == '__main__':
+    sys.exit(main())
